20f0414b002d345aca9507f056941e8fcb619786
Patternscompany <pandureddypatterns@gmail.com>
2025-12-29 17:39:37 +0530

    pp

--- api/server.js
+++ api/server.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
 
 mongoose.connect(process.env.MONGO_URI);
 
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/registration", require("./routes/registration"));
+app.use("/api/auth", require("../routes/auth"));
+app.use("/api/registration", require("../routes/registration"));
 
 app.listen(process.env.PORT || 5000, () => console.log(`Backend running on port ${process.env.PORT || 5000}`));
